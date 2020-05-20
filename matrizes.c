27336670971dab11ef1dd0c273961c4dd5c8e96b
Anthony <anthonyleierlw@gmail.com>
2020-05-20 00:06:39 -0300

    Soma e Subtração

--- matrizes.c
+++ matrizes.c
@@ -20,8 +20,6 @@ Matrix* criar(int lin, int col){
 		}			
 	}
 	
-
-
 	Matrix *A;
 	A=(Matrix*)malloc(sizeof(Matrix));
 	A->matr=mat;
@@ -38,16 +36,78 @@ void imprimir(Matrix* M){
 		}	
 		printf("\n");
 	}
-	return ;
-} 
+	return;
+}
 
+Matrix* preenche(Matrix* M){
+	int i,j;
+	double valor;
+	
+	Matrix *resultado;
+	resultado=(Matrix*)malloc(sizeof(Matrix));
+	
+	resultado = criar(M->linha, M->coluna);
+	resultado->linha=M->linha;
+	resultado->coluna=M->coluna;
+	
+	for(i=0;i<M->linha;i++){
+		for(j=0;j<M->coluna;j++){
+			scanf("%lf", &valor);
+			M->matr[i][j] = valor;
+		}
+	}
+	return;
+}
 
+Matrix* soma(Matrix* M, Matrix* M2){
+	int i,j;
+	
+	Matrix *resultado = (Matrix*)malloc(sizeof(Matrix));
+	
+	resultado = criar(M->linha, M->coluna);
+	resultado->linha=M->linha;
+	resultado->coluna=M->coluna;	
+	
+	for(i=0;i<M->linha;i++){
+		for(j=0;j<M->coluna;j++){
+			resultado->matr[i][j] = M->matr[i][j] + M2->matr[i][j];			
+		}
+	}	
+	
+	return resultado;
+}
 
+Matrix* subtracao(Matrix* M, Matrix* M2){
+	int i,j;
+	
+	Matrix *resultado = (Matrix*)malloc(sizeof(Matrix));
+	
+	resultado = criar(M->linha, M->coluna);
+	resultado->linha=M->linha;
+	resultado->coluna=M->coluna;	
+	
+	for(i=0;i<M->linha;i++){
+		for(j=0;j<M->coluna;j++){
+			resultado->matr[i][j] = M->matr[i][j] - M2->matr[i][j];			
+		}
+	}	
+	
+	return resultado;
+}
 
 int main(){
-	Matrix *M;
-	M=criar(3,3);
-	imprimir(M);
+	Matrix *M, *M1, *M2, *R1, *R2;
+	M1 = criar(3,3);	
+	M2 = criar(3,3);
+	R1 = criar(3,3);	
+	R2 = criar(3,3);
+	preenche(M1);	
+	preenche(M2);	
+	R1 = soma(M1,M2);			
+	R2 = subtracao(M1,M2);		
+	imprimir(R1);
+	printf("\n");
+	imprimir(R2);
 	return 0;
 }
 
