469d76e21f69a0d19b50ead01d26832fdced5abd
Anthony Cruz <anthonyleierlw@gmail.com>
2020-07-10 21:40:39 -0300

    Configuração Gitignore

--- listas.c
+++ listas.c
@@ -1,110 +1,123 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-struct no{
+struct no
+{
 	int valor;
 	struct no *prox;
-
 };
-struct lista{
-	int tam;	
+struct lista
+{
+	int tam;
 	struct no *inicio;
-
 };
 
-typedef struct no NO; 
-typedef struct lista LISTA; 
+typedef struct no NO;
+typedef struct lista LISTA;
 
-
-LISTA* inicializar(){
+LISTA *inicializar()
+{
 	LISTA *L;
-	L=(LISTA*)malloc( sizeof(LISTA) );
-	L->tam=0;
-	L->inicio=NULL;
+	L = (LISTA *)malloc(sizeof(LISTA));
+	L->tam = 0;
+	L->inicio = NULL;
 	return L;
 }
-NO* CriarNo(int valor){
+NO *CriarNo(int valor)
+{
 	NO *N;
-	N=(NO*)malloc( sizeof(NO) );
-	N->valor=valor;
-	N->prox=NULL;
+	N = (NO *)malloc(sizeof(NO));
+	N->valor = valor;
+	N->prox = NULL;
 	return N;
-	
 }
-void InserirInicio(LISTA *L, NO *N){
-	N->prox=L->inicio;
-	L->inicio=N;
-	return;	
-
+void InserirInicio(LISTA *L, NO *N)
+{
+	N->prox = L->inicio;
+	L->inicio = N;
+	return;
 }
-void InserirFim(LISTA *L, NO *N){
+void InserirFim(LISTA *L, NO *N)
+{
 	NO *aux;
-	if(L->inicio==NULL) L->inicio=N;
-	else{
-		for(aux=L->inicio;aux->prox!=NULL;aux=aux->prox);
-		aux->prox=N;		
+	if (L->inicio == NULL)
+		L->inicio = N;
+	else
+	{
+		for (aux = L->inicio; aux->prox != NULL; aux = aux->prox)
+			;
+		aux->prox = N;
 	}
-	return;	
+	return;
 }
 
-void InserirOrdenado(LISTA *L, NO *N){
+void InserirOrdenado(LISTA *L, NO *N)
+{
 	NO *aux;
-	if(L->inicio==NULL) L->inicio=N;
-	else if (N->valor < L->inicio->valor){
+	if (L->inicio == NULL)
+		L->inicio = N;
+	else if (N->valor < L->inicio->valor)
+	{
 		N->prox = L->inicio;
 		L->inicio = N;
 	}
-	else{
-		for(aux=L->inicio; aux->prox != NULL && N->valor >= aux->prox->valor;aux=aux->prox);
-		N->prox=aux->prox;
-		aux->prox=N;	
+	else
+	{
+		for (aux = L->inicio; aux->prox != NULL && N->valor >= aux->prox->valor; aux = aux->prox)
+			;
+		N->prox = aux->prox;
+		aux->prox = N;
 	}
-	return;	
+	return;
 }
 
-LISTA ordernar(LISTA *L){
-	
+LISTA ordernar(LISTA *L)
+{
 }
 
-NO* retirar(LISTA *L, int val){
-
+NO *retirar(LISTA *L, int val)
+{
 }
 
-NO* retirarInicio(LISTA *L){	
+NO *retirarInicio(LISTA *L)
+{
 	NO *aux;
-	
+
 	aux = L->inicio;
 	L->inicio = L->inicio->prox;
 	return aux;
 }
 
-NO* retirarFim(LISTA *L){
+NO *retirarFim(LISTA *L)
+{
 	NO *aux;
 	NO *retirado;
-		
-	for(aux=L->inicio;aux->prox->prox!=NULL;aux=aux->prox);	
+
+	for (aux = L->inicio; aux->prox->prox != NULL; aux = aux->prox)
+		;
 	retirado = aux->prox;
 	aux->prox = NULL;
-	return aux;	
+	return aux;
 }
 
-void imprimir(LISTA *L){
+void imprimir(LISTA *L)
+{
 
 	printf("\nNOSSA LISTA\n");
 	NO *aux;
-	for(aux=L->inicio;aux!=NULL;aux=aux->prox){
-		printf("%d ",aux->valor);
+	for (aux = L->inicio; aux != NULL; aux = aux->prox)
+	{
+		printf("%d ", aux->valor);
 	}
 	return;
 }
 
-
-
-void main(){
+void main()
+{
 
 	LISTA *L;
 	NO *N;
-	L=inicializar();
+	L = inicializar();
 	/*
 	N=CriarNo(10);
 	InserirInicio(L,N);
@@ -130,25 +143,25 @@ void main(){
 	
 	N=CriarNo(8);
 	InserirFim(L,N);
-	*/		
-		
-	N=CriarNo(15);
-	InserirOrdenado(L,N);
-		
-	N=CriarNo(5);
-	InserirOrdenado(L,N);
-	
-	N=CriarNo(3);
-	InserirOrdenado(L,N);	
-	
-	N=CriarNo(6);
-	InserirOrdenado(L,N);	
-	
+	*/
+
+	N = CriarNo(15);
+	InserirOrdenado(L, N);
+
+	N = CriarNo(5);
+	InserirOrdenado(L, N);
+
+	N = CriarNo(3);
+	InserirOrdenado(L, N);
+
+	N = CriarNo(6);
+	InserirOrdenado(L, N);
+
 	imprimir(L);
 	retirarInicio(L);
 	imprimir(L);
 	retirarFim(L);
-	imprimir(L);	
-	
+	imprimir(L);
+
 	return;
 }
