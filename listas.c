a529848b4347bf2adc9d916a85f44ccf11376787
Anthony Cruz <anthonyleierlw@gmail.com>
2020-07-27 19:06:45 -0300

    MatrizesListas Inserções

--- listas.c
+++ listas.c
@@ -1,125 +1,123 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-
-struct no{
-	int valor;
-	struct no *prox;
-
+struct no {
+    int valor;
+    struct no *prox;
 };
-struct lista{
-	int tam;
-	struct no *inicio;
-
+struct lista {
+    int tam;
+    struct no *inicio;
 };
 
 typedef struct no NO;
 typedef struct lista LISTA;
 
-
-LISTA* inicializar(){
-	LISTA *L;
-	L=(LISTA*)malloc( sizeof(LISTA) );
-	L->tam=0;
-	L->inicio=NULL;
-	return L;
+LISTA *inicializar() {
+    LISTA *L;
+    L = (LISTA *)malloc(sizeof(LISTA));
+    L->tam = 0;
+    L->inicio = NULL;
+    return L;
 }
-NO* CriarNo(int valor){
-	NO *N;
-	N=(NO*)malloc( sizeof(NO) );
-	N->valor=valor;
-	N->prox=NULL;
-	return N;
-
+NO *CriarNo(int valor) {
+    NO *N;
+    N = (NO *)malloc(sizeof(NO));
+    N->valor = valor;
+    N->prox = NULL;
+    return N;
 }
-void InserirInicio(LISTA *L, NO *N){
-	N->prox=L->inicio;
-	L->inicio=N;
-	L->tam ++;
-	return;
-
+void InserirInicio(LISTA *L, NO *N) {
+    N->prox = L->inicio;
+    L->inicio = N;
+    L->tam++;
+    return;
 }
-void InserirFim(LISTA *L, NO *N){
-	NO *aux;
-	if(L->inicio==NULL) L->inicio=N;
-	else{
-		for(aux=L->inicio;aux->prox!=NULL ;aux=aux->prox);
-		aux->prox=N;
-
-	}
-	L->tam ++;
-	return;
+void InserirFim(LISTA *L, NO *N) {
+    NO *aux;
+    if (L->inicio == NULL)
+        L->inicio = N;
+    else {
+        for (aux = L->inicio; aux->prox != NULL; aux = aux->prox)
+            ;
+        aux->prox = N;
+    }
+    L->tam++;
+    return;
 }
-void InserirOrdenado(LISTA *L, NO *N){
-	NO *aux;
-	if(L->inicio==NULL) L->inicio=N;
-	else if (N->valor < L->inicio->valor){
-		N->prox = L->inicio;
-		L->inicio = N;
-	}
-	else{
-		for(aux=L->inicio; aux->prox != NULL && N->valor >= aux->prox->valor;aux=aux->prox);
-			N->prox=aux->prox;
-			aux->prox=N;
-	}
-	L->tam ++;
-	return;
+void InserirOrdenado(LISTA *L, NO *N) {
+    NO *aux;
+    if (L->inicio == NULL)
+        L->inicio = N;
+    else if (N->valor < L->inicio->valor) {
+        N->prox = L->inicio;
+        L->inicio = N;
+    } else {
+        for (aux = L->inicio; aux->prox != NULL && N->valor >= aux->prox->valor;
+             aux = aux->prox)
+            ;
+        N->prox = aux->prox;
+        aux->prox = N;
+    }
+    L->tam++;
+    return;
 }
-NO  *retirar(LISTA *L , int val){
+NO *retirar(LISTA *L, int val) {
     NO *ultimo;
     NO *antipenultimo;
-    if (val == L->inicio->valor){
+    if (val == L->inicio->valor) {
         ultimo = L->inicio;
         L->inicio = L->inicio->prox;
-        L->tam --;
+        L->tam--;
         return ultimo;
-	}else{
-        for(ultimo = L->inicio , antipenultimo = L->inicio;  ultimo != NULL &&ultimo->valor < val ; antipenultimo = ultimo , ultimo = ultimo->prox);
-        if (ultimo != NULL && ultimo->valor == val){
+    } else {
+        for (ultimo = L->inicio, antipenultimo = L->inicio;
+             ultimo != NULL && ultimo->valor < val;
+             antipenultimo = ultimo, ultimo = ultimo->prox)
+            ;
+        if (ultimo != NULL && ultimo->valor == val) {
             antipenultimo->prox = ultimo->prox;
-            L->tam --;
+            L->tam--;
             return ultimo;
-        }else {
+        } else {
             return NULL;
         }
-
-	}
-
-}
-
-LISTA *copiar(LISTA *L){ //Implementar copiar uma lista para outra lista
-	LISTA *novo=inicializar();
-	NO *aux;
-	for(aux=L->inicio;aux!=NULL;aux=aux->prox){
-		InserirFim(novo,CriarNo(aux->valor));
-	}
-	return novo;
-}
-
-NO *RetirarInicio(LISTA *L){
-    NO *aux;
-    aux = L->inicio;
-    L->inicio = L->inicio->prox;
-    if(L->tam > 0){
-        L->tam --;
     }
-    aux->prox=NULL;
-    return aux;
 }
-LISTA *ordenar(LISTA *L){
-    NO *aux ;
-    LISTA *novo;
-    novo = inicializar();
-    while(L->inicio != NULL){
-        aux = RetirarInicio(L);
-        InserirOrdenado(novo,aux);
+
+LISTA *copiar(LISTA *L) {  // Implementar copiar uma lista para outra lista
+    LISTA *novo = inicializar();
+    NO *aux;
+    for (aux = L->inicio; aux != NULL; aux = aux->prox) {
+        InserirFim(novo, CriarNo(aux->valor));
     }
     return novo;
 }
 
-NO *RetirarFim(LISTA *L){
-     NO *ultimo ;
-     NO *antipenultimo;
+NO *RetirarInicio(LISTA *L) {
+    NO *aux;
+    aux = L->inicio;
+    L->inicio = L->inicio->prox;
+    if (L->tam > 0) {
+        L->tam--;
+    }
+    aux->prox = NULL;
+    return aux;
+}
+LISTA *ordenar(LISTA *L) {
+    NO *aux;
+    LISTA *novo;
+    novo = inicializar();
+    while (L->inicio != NULL) {
+        aux = RetirarInicio(L);
+        InserirOrdenado(novo, aux);
+    }
+    return novo;
+}
+
+NO *RetirarFim(LISTA *L) {
+    NO *ultimo;
+    NO *antipenultimo;
     /*
      NO *ultimo = L;
      NO *antipenultimo= L;
@@ -128,60 +126,54 @@ NO *RetirarFim(LISTA *L){
         ultimo = ultimo->prox;
 
     }*/
-    for(ultimo = L->inicio , antipenultimo = L->inicio;ultimo->prox !=NULL; antipenultimo = ultimo , ultimo = ultimo->prox);
+    for (ultimo = L->inicio, antipenultimo = L->inicio; ultimo->prox != NULL;
+         antipenultimo = ultimo, ultimo = ultimo->prox)
+        ;
     antipenultimo->prox = NULL;
-   
-   if(L->tam > 0){
-        L->tam --;
+
+    if (L->tam > 0) {
+        L->tam--;
     }
     return ultimo;
 }
 
-
-void imprimir(LISTA *L){
-
-	printf("NOSSA LISTA\t");
-	NO *aux;
-	for(aux=L->inicio;aux!=NULL;aux=aux->prox){
-		printf("%d ",aux->valor);
-	}
-	printf("\n");
-	return;
-
+void imprimir(LISTA *L) {
+    printf("NOSSA LISTA\t");
+    NO *aux;
+    for (aux = L->inicio; aux != NULL; aux = aux->prox) {
+        printf("%d ", aux->valor);
+    }
+    printf("\n");
+    return;
 }
 
+void main() {
+    LISTA *L, *novo;
+    NO *N;
+    L = inicializar();
 
+    N = CriarNo(15);
+    InserirInicio(L, N);
+    // InserirOrdenado(L,N);
+    N = CriarNo(5);
+    InserirInicio(L, N);
+    // InserirOrdenado(L,N);
+    N = CriarNo(3);
+    InserirInicio(L, N);
+    // InserirOrdenado(L,N);
+    N = CriarNo(6);
+    InserirInicio(L, N);
+    // InserirOrdenado(L,N);
+    // RetirarFim(L);
+    // N = retirar(18,L);
+    imprimir(L);
 
-void main(){
+    L = ordenar(L);
 
-	LISTA *L,*novo;
-	NO *N;
-	L=inicializar();
+    novo = copiar(L);
+    RetirarFim(L);
+    imprimir(L);
+    imprimir(novo);
 
-	N=CriarNo(15);
-    InserirInicio(L,N);
-    //InserirOrdenado(L,N);
-	N=CriarNo(5);
-    InserirInicio(L,N);
-    //InserirOrdenado(L,N);
-	N=CriarNo(3);
-    InserirInicio(L,N);
-    //InserirOrdenado(L,N);
-	N=CriarNo(6);
-	InserirInicio(L,N);
-	//InserirOrdenado(L,N);
-	//RetirarFim(L);
-	//N = retirar(18,L);
-	imprimir(L);
-	
-	
-	
-	L = ordenar(L);
-	
-	novo=copiar(L);
-	RetirarFim(L);
-	imprimir(L);
-	imprimir(novo);
-
-	return ;
+    return;
 }
--- matrizesListas.c
+++ matrizesListas.c
@@ -1,87 +1,127 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-
-struct no{
-	double valor;
-	int line;
-	int column;
-	struct no *linha;
-	struct no *coluna;
-
+struct no {
+    int line;
+    int column;
+    double info;
+    struct no *right;
+    struct no *below;
 };
-typedef struct no matrix;
+typedef struct no Matrix;
 
-
-matrix* CriarN0(int l,int c){
-	matrix *N=(matrix*)malloc(sizeof(matrix));
-	N->valor=0;
-	N->line=l;
-	N->column=c;
-	N->linha=NULL;
-	N->coluna=NULL;
-	return N;
+Matrix *CriarN0(int l, int c) {
+    Matrix *N = (Matrix *)malloc(sizeof(Matrix));
+    N->info = 0;
+    N->line = l;
+    N->column = c;
+    N->right = NULL;
+    N->below = NULL;
+    return N;
 }
 
-
-
-
-matrix *zeros(int n,int m){
-	matrix *M;
-	M=(matrix*)malloc(sizeof(matrix));
-	M->line=n;
-	M->column=m;
-	M->linha=M;
-	M->coluna=M;
-	int i;
-	matrix* aux;
-	for(i=n;i>0;i--){
-		aux=CriarN0(i,-1);
-		aux->linha=M->linha;
-		M->linha=aux;
-		aux->coluna=aux;
-	}
-	for(i=m;i>0;i--){
-		aux=CriarN0(-1,i);
-		aux->coluna=M->coluna;
-		M->coluna=aux;
-		aux->linha=aux;
-	}
-	return M;
+Matrix *zeros(int n, int m) {
+    Matrix *M;
+    M = (Matrix *)malloc(sizeof(Matrix));
+    M->line = n;
+    M->column = m;
+    M->right = M;
+    M->below = M;
+    int i;
+    Matrix *aux;
+    for (i = n; i > 0; i--) {
+        aux = CriarN0(i, -1);
+        aux->below = M->below;
+        M->below = aux;
+        aux->right = aux;
+    }
+    for (i = m; i > 0; i--) {
+        aux = CriarN0(-1, i);
+        aux->right = M->right;
+        M->right = aux;
+        aux->below = aux;
+    }
+    return M;
 }
 
+// arrumar tratamento de erro se a posicao ja existe
+void matrix_setelem(Matrix *M, int posicaoi, int posicaoj, float novo_valor) {
+    int k, i, j;
+    Matrix *aux2, *aux3;
+    Matrix *aux = CriarN0(posicaoi, posicaoj);
+    aux->info = novo_valor;
 
-matrix* inserir(matrix *M,matrix *no){
+    // apontar o aux para o inicio
+    if (posicaoi < 1 || posicaoj < 1 || posicaoi > M->line ||
+        posicaoj > M->column) {
+        printf("ERRO, posicao invalida!!!\n");
+        free(aux);
+        return;
+    } else {
+        // fazendo ele percorrer atÃ© a posicao que eu quero colocar
+        // o valor
+        // tratamento de erro se a posicao jÃ¡ conter um valor
+        aux2 = M;
+        for (i = 0; i < posicaoi; i++) {
+            aux2 = aux2->right;
+        }
+        for (aux3 = aux2, aux2 = aux2->below;
+             aux2->line < posicaoj && aux2->line != -1;
+             aux3 = aux2, aux2 = aux2->below)
+            ;
+        aux->below = aux2;
+        aux3->below = aux;
 
+        aux2 = M;
+        for (i = 0; i < posicaoj; i++) {
+            aux2 = aux2->below;
+        }
 
+        for (aux3 = aux2, aux2 = aux2->right;
+             aux2->column < posicaoi && aux2->column != -1;
+             aux3 = aux2, aux2 = aux2->right)
+            ;
+        aux->right = aux2;
+        aux3->right = aux;
+    }
 
+    return;
 }
 
+// retorna o valor do elemento (x, y) da matriz m.
+float matrix_getelem(Matrix *m, int x, int y) {}
 
-void imprimir(matrix *M){
-
-
-
-
+void imprimir(Matrix *M) {
+    int i, j;
+    Matrix *aux;
+    aux = M->below;
+    printf("IMPRESSAO!!\n");
+    for (aux = M->below; aux != M; aux = aux->below) {
+        for (aux = aux->right; aux->column != -1; aux = aux->right) {
+            printf("%d %d %lf \n", aux->line, aux->column, aux->info);
+        }
+    }
 }
 
+void main() {
+    Matrix *M, *aux;
+    M = zeros(3, 4);
 
+    matrix_setelem(M, 1, 1, 50.0);
+    matrix_setelem(M, 1, 1, 255.0);
+    matrix_setelem(M, 2, 1, 655.0);
+    matrix_setelem(M, 4, 4, 755.55);
 
-void main(){
+    /*
+    for(aux=M->right;aux!=M;aux=aux->right){
+            printf("%lf %d %d \n",aux->info,aux->line,aux->column);
+    }
+    for(aux=M->below;aux!=M;aux=aux->below){
+            printf("%lf %d %d \n",aux->info,aux->line,aux->column);
+    }
+    */
 
-	matrix *M;
-	M=zeros(3,4);
-	
-	
-	matrix *aux;
-	for(aux=M->linha;aux!=M;aux=aux->linha){
-		printf("%lf %d %d \n",aux->valor,aux->line,aux->column);
-	}
-	for(aux=M->coluna;aux!=M;aux=aux->coluna){
-		printf("%lf %d %d \n",aux->valor,aux->line,aux->column);
-	}
+    imprimir(M);
 
-	return;
+    return;
 }
-
-
